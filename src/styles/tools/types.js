5c0871befb0ef9a9797eb4aba3db84925c22d17f
Gustavo Teodoro <gteodoro@hugeinc.com>
2017-12-14 15:45:48 -0200

    feat: Adding types tools

---
+++ src/styles/tools/types.js
@@ -0,0 +1,51 @@
+export function josefinRegular(fontSize) {
+    return `
+        font-family: 'Josefin Sans', sans-serif;
+        font-size: ${fontSize};
+        font-weight: 400;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
+
+export function ubuntuRegular(fontSize) {
+    return `
+        font-family: 'Ubuntu', sans-serif;
+        font-size: ${fontSize};
+        font-weight: 400;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
+
+export function ubuntuRegularItalic(fontSize) {
+    return `
+        font-family: 'Ubuntu', sans-serif;
+        font-size: ${fontSize};
+        font-style: italic;        
+        font-weight: 400;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
+
+export function ubuntuBold(fontSize) {
+    return `
+        font-family: 'Ubuntu', sans-serif;
+        font-size: ${fontSize};
+        font-weight: 700;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
+
+export function ubuntuBoldItalic(fontSize) {
+    return `
+        font-family: 'Ubuntu', sans-serif;
+        font-size: ${fontSize};
+        font-style: italic;        
+        font-weight: 700;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    `;
+}
