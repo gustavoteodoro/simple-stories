54f322353e27477daf92e390adecc95684f56da0
Gustavo Teodoro <gteodoro@hugeinc.com>
2017-12-14 15:40:47 -0200

    feat: Creating colors settings

---
+++ src/styles/settings/colors.js
@@ -0,0 +1,6 @@
+export const white = '#ffffff';
+export const black = '#000000';
+export const lightGrey = '#ececec';
+export const darkBlueGray = '#687899';
+export const japeneseIndigo = '#19435C';
+export const darkSeaGreen = '#82C387';
