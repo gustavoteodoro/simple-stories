878de3f848999439f1914d4cf77aff663c7e2525
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2017-12-27 21:44:20 -0200

    fix: Organizing files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Home from './pages/Home';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
