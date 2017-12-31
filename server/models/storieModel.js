4ca4c09c75460c33ef0932acbd35eb2be0992ec9
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2017-12-31 19:04:38 -0200

    feat: Creating storieModel schema

---
+++ server/models/storieModel.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var storieSchema = new Schema ({
+  storieSlug: String,
+  storieTitle: String,
+  storieAuthor: String,
+  storieCover: String,
+  storieAuthor: String,
+  storieDate: {type: Date, default: Date.now},
+});
+
+var StorieModel = mongoose.model('Course', storieSchema);
+
+module.exports = StorieModel;
