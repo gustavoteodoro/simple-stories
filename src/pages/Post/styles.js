e16264f9e625ff325f029e03bf61ab51dee50a0e
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2017-12-31 18:22:59 -0200

    feat: Style for post

--- src/pages/Post/styles.js
+++ src/pages/Post/styles.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components';
 
+import {
+    ubuntuRegular,
+    ubuntuBold,
+    spectralRegular,
+} from '../../styles/tools/types';
+
+import {
+    japeneseIndigo,
+    darkSeaGreen,
+    white,
+} from '../../styles/settings/colors';
+
 export const PostContainer = styled.div`
     display: block;
+    background: ${white};
 `;
 
 export const PostContent = styled.article`
     max-width: 1200px;
     margin: 0 auto;
 `
+
+export const PostTitle = styled.h1`
+    ${ubuntuBold(120)}
+    
+    color: ${japeneseIndigo};
+    margin: 0;
+    padding: 20px 0;
+`;
+
+export const PostCover = styled.img`
+    display: block;
+    width: 100%;
+`;
+
+export const PostText = styled.div`
+    ${ubuntuRegular(24)}
+
+    line-height: 50px;
+    margin: 60px auto;
+    max-width: 900px;
+    text-align: justify;
+
+    p {
+        &:first-child{
+            &:first-letter{
+                ${spectralRegular(360)}
+                color: ${darkSeaGreen};    
+                margin-left: -150px;
+            }
+        }
+    }
+`;
