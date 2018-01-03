021d704358b6ec4afa3dac5b66b5d956604f9f73
Gustavo Teodoro <gustavoalvesteodoro@gmail.com>
2018-01-03 03:51:27 -0200

    fix: Fixing server identation

--- server/server.js
+++ server/server.js
@@ -10,25 +10,25 @@ var UserModel = require('./models/userModel');
 var StorieModel = require('./models/storieModel');
 
 passport.use(new Strategy(
-    function(username, password, cb) {
-        UserModel.findOne({ userEmail: username }, function(err, user) {
-            if (err) { return cb(err); }
-            if (!user) { return cb(null, false); }
-            if (user.userPassword != password) { return cb(null, false); }
-            return cb(null, user);
-        });
-    }
+  function(username, password, cb) {
+    UserModel.findOne({ userEmail: username }, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.userPassword != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }
 ));
 
 passport.serializeUser(function(user, cb) {
-    cb(null, user._id);
+  cb(null, user._id);
 });
   
 passport.deserializeUser(function(id, cb) {
-    UserModel.findOne({ _id: id }, function(err, user) {
-        if (err) { return cb(err); }
-        cb(null, user);
-    });
+  UserModel.findOne({ _id: id }, function(err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
 });
   
 app.use(require('morgan')('combined'));
@@ -40,91 +40,91 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.post('/api/login',
-    passport.authenticate('local'),
-    function (req, res) {
-        res.json({
-            "user": req.user,
-        })
-    }
+  passport.authenticate('local'),
+  function (req, res) {
+    res.json({
+      "user": req.user,
+    })
+  }
 );
 
 app.get('/api/auth', function (req, res) {
-    res.json({
-        "user": req.user
-    })
+  res.json({
+    "user": req.user
+  })
 });
 
 app.get('/api/logout', function (req, res) {
-    req.session.destroy(function (err) {
-        res.json("Logout successfully")
-    })
+  req.session.destroy(function (err) {
+    res.json("Logout successfully")
+  })
 });
 
 app.post('/api/create-account', function (req, res) {
-    UserModel.findOne({ userEmail: req.body.email }, function(err, user) {
-        if(user){
-            res.json('Email already registerd.');
-        } else {
-            var user = new UserModel({
-                userName: req.body.name,
-                userEmail: req.body.email,
-                userPassword: req.body.password,
-            });
-            user.save(function(error, user){
-                if(error) return console.error(error);
-                res.json('User created successfully.');
-            })
-        }
-    });
+  UserModel.findOne({ userEmail: req.body.email }, function(err, user) {
+    if(user){
+      res.json('Email already registerd.');
+    } else {
+      var user = new UserModel({
+        userName: req.body.name,
+        userEmail: req.body.email,
+        userPassword: req.body.password,
+      });
+      user.save(function(error, user){
+        if(error) return console.error(error);
+        res.json('User created successfully.');
+      })
+    }
+  });
 });
 
 app.get('/api/stories', function (req, res) {
-    StorieModel.find().exec(function(error, stories){
-        if(error) return console.error(error);
-        res.json({
-            "stories": stories
-        })
+  StorieModel.find().exec(function(error, stories){
+    if(error) return console.error(error);
+    res.json({
+      "stories": stories
     })
+  })
 });
 
 app.get('/api/storie/:slug', function (req, res) {
-    StorieModel.findOne({ storieSlug: req.params.slug }, function(err, storie) {
-        if(storie){
-            res.json({
-                "storie": storie
-            })
-        }
-    });
+  StorieModel.findOne({ storieSlug: req.params.slug }, function(err, storie) {
+    if(storie){
+      res.json({
+        "storie": storie
+      })
+    }
+  });
 });
 
 app.get('/api/create-storie',
-    function(req, res){
-        res.json({
-            "user": req.user
-        })
-    }
+  function(req, res){
+    res.json({
+      "user": req.user
+    })
+  }
 );
 
 app.post('/api/create-storie', function(req, res){
-    StorieModel.findOne({ storieSlug: req.body.storieSlug }, function(err, storie) {
-        let newStorieSlug = req.body.storieSlug;
-        if(storie){
-            newStorieSlug = req.body.storieSlug + Math.floor((Math.random() * 100) + 1);
-        }
-        var storie = new StorieModel({
-            storieSlug: newStorieSlug,
-            storieTitle: req.body.storieTitle,
-            storieCover: 'cover' + Math.floor((Math.random() * 10) + 1) + '.jpg',
-            storieAuthor: req.body.storieAuthor,
-            storieText: req.body.storieText
-        });
-        storie.save(function(error, storie){
-            if(error) return console.error(error);
-            res.json('Storie created.');
-        })
+  StorieModel.findOne({ storieSlug: req.body.storieSlug }, function(err, storie) {
+    let newStorieSlug = req.body.storieSlug;
+    if(storie){
+      newStorieSlug = req.body.storieSlug + Math.floor((Math.random() * 100) + 1);
+    }
+    var storie = new StorieModel({
+      storieSlug: newStorieSlug,
+      storieTitle: req.body.storieTitle,
+      storieCover: 'cover' + Math.floor((Math.random() * 10) + 1) + '.jpg',
+      storieAuthor: req.body.storieAuthor,
+      storieText: req.body.storieText
     });
+    storie.save(function(error, storie){
+      if(error) return console.error(error);
+      res.json('Storie created.');
+    })
+  });
 });
 
 app.listen(3001, function () {
-    console.log('Server running on port 3001');
+  console.log('Server running on port 3001');
 });
